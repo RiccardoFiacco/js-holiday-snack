f46c37ee6082adbfe17bd8ac777a72ec890cae32
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-02 00:24:47 +0100

    add print and change array method

--- js/script.js
+++ js/script.js
@@ -7,14 +7,15 @@ const firstForm = document.getElementById("first-snack-form");
 firstForm.addEventListener("submit", (event)=>{
     event.preventDefault();
     const arrWord = document.getElementById("input-first-snack").value.toLowerCase().split('');
-    console.log(arrWord)
+
     const newArr = arrWord.map((letter)=>{
         if(letter== 'a'|| letter== 'e' || letter== 'i'|| letter== 'o'|| letter== 'u'){
             letter = letter+'f'+letter
-            console.log(letter)
         }
         return letter
     })
-    newArr.concat();
-    
+    const print = newArr.join('');
+
+    const displayFirst = document.getElementById("result-first-snack");
+    displayFirst.innerText = print;
 })
