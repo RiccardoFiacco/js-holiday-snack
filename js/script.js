a668da2faaf612db8f4124589405eacbe9232313
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-03 00:37:38 +0100

    add correction to for loop

--- js/script.js
+++ js/script.js
@@ -174,7 +174,7 @@ sixthForm.addEventListener("submit", (event)=>{
 //seventh snack
 
 const hiddenFirstSnack = document.getElementById("sherlock-first-form");
-const firstSherlockArray = [];
+let firstSherlockArray = [];
 
 insertSherlockFirstSnack.addEventListener("click", (event)=>{
     event.preventDefault();
@@ -186,10 +186,10 @@ insertSherlockFirstSnack.addEventListener("click", (event)=>{
 hiddenFirstSnack.addEventListener(("submit"), (event)=>{
     event.preventDefault();
     let sum = 0;
-    for(let i = 0; i< firstSherlockArray ; i++){
-        sum += firstSherlockArray;
+    for(let i = 0; i < firstSherlockArray.length; i++){
+        sum += firstSherlockArray[i];
     }
-    let result = sum/2;
+    let result = sum / 2;
     resultSherlockFirstSnack.innerText = result;
     firstSherlockArray = [];
 })
