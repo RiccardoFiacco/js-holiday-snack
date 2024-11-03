726c286094858ee7723b6e056cdc7071c26cde2a
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-03 16:04:49 +0100

    add solution to last snack

--- eleventh/js/script.js
+++ eleventh/js/script.js
@@ -1,3 +1,36 @@
+function check(el){
+    for(let i = 0; word[i]==word[i+1] ; i++) {
+        console.log("lettera uguale")
+    }
+}
+
 form.addEventListener("submit", (event)=>{
     event.preventDefault();
+    let word = document.getElementById("text").value.split('');
+    let finalWord = '';
+    let index = 0
+
+    for(let i = 0; i<word.length;){
+        let res = count(word[i]);
+            if(res<=1){           
+                finalWord += word[i];
+            }else{
+                finalWord += word[i] + res;
+            }
+            i+=res
+    }
+    
+
+    console.log(finalWord)
+
+
+
+    function count(el){
+        let count = 0;
+        for (index; el == word[index]; index++) {     
+            count++;
+        }
+        return count;
+    }
+
 })
