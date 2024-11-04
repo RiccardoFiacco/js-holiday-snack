d55c4af0cb7fd23f36b3654ac095abc80bb56272
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-04 11:21:31 +0100

    removed first snack

--- js/script.js
+++ js/script.js
@@ -35,27 +35,6 @@ function isDivisible(arr, divisor){
          return ("ce una stringa nell'array");
     }  
 }
-//first snack
-
-const firstForm = document.getElementById("first-snack-form");
-
-firstForm.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    const arrWord = document.getElementById("input-first-snack").value.toLowerCase().split('');
-
-    const newArr = arrWord.map((letter)=>{
-        if(letter== 'a'|| letter== 'e' || letter== 'i'|| letter== 'o'|| letter== 'u'){
-            letter = letter+'f'+letter
-        }
-        return letter
-    })
-    const print = newArr.join('');
-
-    const displayFirst = document.getElementById("result-first-snack");
-    displayFirst.innerText = print;
-})
-
-
 //secondSnack
 
 const secondForm = document.getElementById("second-snack-form");
