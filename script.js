5f96ef82d37c2128ca6bfd5a133ff750b657e8b3
RiccardoFiacco <riccardofiacco17@gmail.com>
2024-11-04 17:32:02 +0100

    removed seventh script

--- script.js
+++ script.js
@@ -37,28 +37,6 @@ function isDivisible(arr, divisor){
     }  
 }
 export {isNum, isEven,isDivisible}
-// //seventh snack
-
-// const hiddenFirstSnack = document.getElementById("sherlock-first-form");
-// let firstSherlockArray = [];
-
-// insertSherlockFirstSnack.addEventListener("click", (event)=>{
-//     event.preventDefault();
-//     const num = parseInt(document.getElementById("input-sherlock-first-snack").value);
-//     firstSherlockArray.push(num); 
-//     document.getElementById("input-sherlock-first-snack").value = "";
-// })
-
-// hiddenFirstSnack.addEventListener(("submit"), (event)=>{
-//     event.preventDefault();
-//     let sum = 0;
-//     for(let i = 0; i < firstSherlockArray.length; i++){
-//         sum += firstSherlockArray[i];
-//     }
-//     let result = sum / 2;
-//     resultSherlockFirstSnack.innerText = result;
-//     firstSherlockArray = [];
-// })
 
 // //eighth snack
 
